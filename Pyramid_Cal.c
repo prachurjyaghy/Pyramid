fbde169998510390c6b6b9bac894ab9e468f8cea
prachurjyaghy <48976708+prachurjyaghy@users.noreply.github.com>
2019-12-06 20:57:14 +0530

    Create Pyramid_Cal.c

---
+++ Pyramid_Cal.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void main()
+{
+    int i,j,k,rows;
+    scanf("%d",&rows);
+      for(i=1;i<=rows;i++)
+        {
+        for(j=i;j<=rows;j++)
+            printf(" ");
+
+        for(k=1;k<=(2*i-1);k++)
+
+         printf("%ld",i);
+
+        printf("\n");
+        }
+        return 0;
+}
+
